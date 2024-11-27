8abcab0351f8403b33aab74d36ee63ad0fd77f41
sibaram1993 <sibarammahapatra.kpt@gmail.com>
2024-11-27 14:32:07 +0530

    Update AssignmentTestng.java

--- AssignmentTestng.java
+++ AssignmentTestng.java
@@ -24,7 +24,7 @@ public class AssignmentTestng {
 	@BeforeTest
 	public void browserSetup() throws Throwable {
 		
-		System.setProperty("webdriver.gecko.driver","C:\\Users\\user\\Eclips2\\assignment\\rc\\geckodriver.exe");
+		System.setProperty("webdriver.gecko.driver","C:\\Users\\user\\git\\FitPeo\\assignment\\rc\\geckodriver.exe");
 		 dr = new FirefoxDriver();
 		Thread.sleep(3000);
 	}
